038c451f3c0214d9ec16efffc9b3baf7a799d622
Tracey <radcliffe.tracey@gmail.com>
2016-07-20 10:28:36 -0700

    finished cli test

--- lab-tracey-radcliffe/bin/readFilesCLI.js
+++ lab-tracey-radcliffe/bin/readFilesCLI.js
@@ -2,11 +2,10 @@
 
 var readFiles = require('../lib/readFiles');
 
-var readFilesCLI = function(writeStream) {
+var readFilesCLI = exports = module.exports = function(writeStream) {
   var files = process.argv.slice(2);
   writeStream = writeStream || process.stdout;
   readFiles(files, function(err, data) {
-    debugger;
     if(err) return writeStream.write(err);
     data.forEach(function(data){
       writeStream.write(data.toString());
--- lab-tracey-radcliffe/test/readfiles_test.js
+++ lab-tracey-radcliffe/test/readfiles_test.js
@@ -1,14 +1,39 @@
 const expect = require('chai').expect;
 const readFile = require('../lib/readFiles');
+const readFilescli = require('../bin/readFilesCLI');
 
 var files = ['test/test1.txt', 'test/test2.txt', 'test/test3.txt'];
 
-describe('checking the readFiles is working', function() {
+describe('checking the readFiles is working', () => {
   it('the files should come out in the right order', function(done) {
     readFile(files, function(err, data){
       console.log(data);
       expect(data).to.eql(['this is test 1\n', 'this is test 2\n', 'this is test 3\n' ]);
       done();
     });
+  });
+  it('checks to make sure in output order is the same as the input order', () => {
+
+  });
+});
+
+describe('read cli', () =>{
+  before(()=>{
+    this.argv_backup = process.argv;
+    process.argv = [null, null, 'test'];
+  });
+
+  after(() => {
+    process.argv = this.argv_backup;
+  });
+
+  it('shopuld read from process.argv', () => {
+    var called = false;
+    var testStream = {
+      write: function(input){
+        expect(input).to.eql(readFile('test'));
+      }
+    };
+    readFilescli(undefined, testStream);
   });
 });
